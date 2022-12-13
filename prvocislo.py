e6ff8e0aa9bb189b8eb1951401c5fe4780839068
Lukáš Koudelík <120519296+lukaskoudelik@users.noreply.github.com>
2022-12-13 23:00:01 +0100

    Update prvocislo.py
    Add function power

--- prvocislo.py
+++ prvocislo.py
@@ -15,7 +15,32 @@ def counting_prime(input):
             return isPrime(input, 3)
 
 
+#Fermat's factorization method
+
+# Iterative Function to calculate
+# (a^n)%p in O(logy)
 def power(a, n, p):
+     
+    # Initialize result
+    result = 1
+     
+    # Update 'a' if 'a' >= p
+    a = a % p 
+     
+    while n > 0:
+         
+        # If n is odd, multiply
+        # 'a' with result
+        if n % 2:
+            result = (result * a) % p
+            n = n - 1
+        else:
+            a = (a ** 2) % p
+             
+            # n must be even now
+            n = n // 2
+             
+    return result % p
 
 
 def isPrime(n, k):
